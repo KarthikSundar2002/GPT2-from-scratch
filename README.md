5aa16e1defe8a0d77df9cfa02507cf042ddf9467
KarthikSundar2002 <karthiksundar30092002@gmail.com>
2025-11-05 13:32:38 +0000

    feat: Adds torch.compile

--- README.md
+++ README.md
@@ -5,5 +5,13 @@ Started with Batch Size = 2, Seq Length = 1024.
 Running on a A4000 (I know it is pretty old, but that's all I have access to :( )
 
 Training for 1 epoch
-Without any Optimizations - Time to train: 48.72043180465698 seconds
-float32_matmul_precision - high - Allowing for TF32 - Time to train: 29.83514142036438 seconds
+1. Without any Optimizations - Time to train: 48.72043180465698 seconds
+2. float32_matmul_precision - high - Allowing for TF32 - Time to train: 29.83514142036438 seconds
+3. torch.autocast to bfloat16 + (2) - Time to train: 24.927905797958374 seconds
+4. With torch.compile + (3) - Time to train: 32.85438013076782 seconds
+
+For 5 epochs,
+
+1. With torch.compile - Time to train: 81.19576597213745 seconds
+2. with torch.autocast to bfloat16 and float32 matmul precision to high - Time to train: 125.25839257240295 seconds
+
--- train.py
+++ train.py
@@ -7,6 +7,7 @@ from config import GPTConfig
 from GPT2 import GPT
 from dataloader import DataLoader
 
+NUM_EPOCHS = 5
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using device: {device}")
 
@@ -18,6 +19,7 @@ torch.set_float32_matmul_precision("high")
 model = GPT(GPTConfig())
 start_time = time.time()
 m = model.to(device).train()
+m = torch.compile(m)
 end_time = time.time()
 print(f"Time to load model: {end_time - start_time} seconds")
 
@@ -25,14 +27,15 @@ optimizer = torch.optim.AdamW(m.parameters(), lr=3e-4)
 
 dataloader = DataLoader(B=2, T=1024)
 start_time = time.time()
-for i in range(len(dataloader)):
-    x, y = next(dataloader)
-    with torch.autocast(device_type=device, dtype=torch.bfloat16):
-         logits, loss = m(x.to(device), y.to(device))
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-    print(f"Step {i}: loss = {loss.item()}")
+for epoch in range(NUM_EPOCHS):
+    for i in range(len(dataloader)):
+        x, y = next(dataloader)
+        with torch.autocast(device_type=device, dtype=torch.bfloat16):
+            logits, loss = m(x.to(device), y.to(device))
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        print(f"Epoch {epoch}, Step {i}: loss = {loss.item()}")
 torch.cuda.synchronize()
 end_time = time.time()
 print(f"Time to train: {end_time - start_time} seconds")
